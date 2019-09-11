a12c90239efc9c76243a237c6b312ae415342e26
swapnilvijaykumarbamb <swapnilbamb1204@gmail.com>
2019-09-11 10:35:41 +0530

    [swapnil] Add. database configuration done

--- backend/server.js
+++ backend/server.js
@@ -10,3 +10,6 @@ var http = require('http').Server(app);
 var server = http.listen(process.env.port, function () {
     console.log(`listening on localhost:${process.env.port}`);
 });
+/** Configuring the database. */
+const dbConfig = require('../backend/configs/Database.config.js');
+dbConfig.connection();
