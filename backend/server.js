49267c11d8cda1ab55af4b373fd4ff6019a361b8
swapnilvijaykumarbamb <swapnilbamb1204@gmail.com>
2019-09-11 10:22:32 +0530

    [swapnil] Add. server is ready to serve on port

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+require('dotenv').config();
+var http = require('http').Server(app);
+var server = http.listen(process.env.port, function () {
+    console.log(`listening on localhost:${process.env.port}`);
+});
