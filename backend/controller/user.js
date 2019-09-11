c11aa707b583126d8f147f220700cb1884856a58
swapnilvijaykumarbamb <swapnilbamb1204@gmail.com>
2019-09-11 20:50:23 +0530

    [swapnil] Add. functionality to check return is true

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,5 +1,11 @@
 exports.login = (req, res) => {
     var response = {};
+    response.success = true;
     response.message = "login successfull";
+    var result = {
+        username:"swapnil.bamb@Thoughtworks.com",
+        password:"bamb"
+    }
+    response.data = result;
     res.status(200).send(response);
 }
--- backend/test/user.js
+++ backend/test/user.js
@@ -6,22 +6,41 @@ chai.use(chaiHttp);
 const server = require('../server');
 describe('/post login ', () => {
     it(" Login function send expected result", (done) => {
-        let data = {username: "swapnil.bamb@Thoughtworks.com", password: "swapnil@1111S"}
+        let data = {
+            username: "swapnil.bamb@Thoughtworks.com",
+            password: "swapnil@1111S"
+        }
         chai.request(server)
             .post('/login')
             .send(data)
             .end((err, res) => {
-                assert.equal(200,res.status);
+                assert.equal(200, res.status);
             })
         done();
     })
     it(" Login function send expected message", (done) => {
-        let data = {username: "swapnil.bamb@Thoughtworks.com", password: "swapnil@1111S"}
+        let data = {
+            username: "swapnil.bamb@Thoughtworks.com",
+            password: "swapnil@1111S"
+        }
         chai.request(server)
             .post('/login')
             .send(data)
             .end((err, res) => {
-                assert.equal(res.body.message,"login successfull");
+                assert.equal(res.body.message, "login successfull");
+            })
+        done();
+    })
+    it(" Login function return true", (done) => {
+        let data = {
+            username: "swapnil.bamb@Thoughtworks.com",
+            password: "swapnil@1111S"
+        }
+        chai.request(server)
+            .post('/login')
+            .send(data)
+            .end((err, res) => {
+                assert.isTrue(res.body.success);
             })
         done();
     })
