e2880505f84b0346f73c21b3a9545830b0f5bb0d
swapnilvijaykumarbamb <swapnilbamb1204@gmail.com>
2019-09-12 08:19:59 +0530

    [swapnil] Add. primary angular project setup

---
+++ frontend/src/app/app-material.module.ts
@@ -0,0 +1,14 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppMaterialModule { }
