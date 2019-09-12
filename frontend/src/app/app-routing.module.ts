89f438496135884ea95a073e29d9ba6816f7bd9b
swapnilvijaykumarbamb <swapnilbamb1204@gmail.com>
2019-09-12 09:21:24 +0530

    [swapnil] Add. capable to route login component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-const routes: Routes = [];
+import { LoginComponent } from './component/login/login.component';
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
